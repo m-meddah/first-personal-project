4c523e2fc100d0d10d14dca09eb3bbf600a1af1e
m-meddah <meddah.madjid@gmail.com>
2022-04-03 19:12:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const express = require("express");
+const app = express();
+
+app.set("views", "app/views");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+const session = require("express-session");
+app.use(
+  session({
+    saveUninitialized: true,
+    resave: true,
+    secret: "lqhmkmnvfjklnwmsjvmsld",
+  })
+);
+
+const router = require("app/router");
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Listening on : http://localhost:${PORT}`);
+});
